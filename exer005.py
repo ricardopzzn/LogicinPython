7358ed6fc9172385bcc4f3010706e86a6cc1bf45
Ricardo Moureira <111253681+ricardopzzn@users.noreply.github.com>
2025-04-03 20:41:13 -0400

    commit inicial

---
+++ exer005.py
@@ -0,0 +1,11 @@
+print('Antecessor e Sucessor') # Desafio do dia.
+
+print(20 *'=-')
+print('     Antecessor e Sucessor')
+print(20 * '=-')
+n = int(input('Digite um número: '))
+print(f'O número digitado foi {n}')
+ant = n - 1
+suc = n + 1
+print(f'O número antecesor é {ant}')
+print(f'O número sucessor é {suc}')
