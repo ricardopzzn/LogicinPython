fa808ffb3c070c6bcd31e63c5f209bb14be8421c
Ricardo Moureira <111253681+ricardopzzn@users.noreply.github.com>
2025-05-01 19:00:38 -0400

    exer097 func

---
+++ exer097.py
@@ -0,0 +1,9 @@
+'''
+Exercício Python 097: Faça um programa que tenha uma função chamada escreva()
+que receba um texto qualquer como parâmetro e mostre uma mensagem com tamanho adaptável.  
+'''
+def escreva(val):
+    return print(val)
+
+
+escreva('Ricardo')
