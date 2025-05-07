e41f8dc1dca4abd3f653ee5951862e950e656fc2
ricardopzzn <ricardopzzn@gmail.com>
2025-05-07 19:19:11 -0400

    exer101 add

--- exer101.py
+++ exer101.py
@@ -1,5 +1,22 @@
-def soma(a, b):
-    return a * b
+def votacao(nasc):
+    """
+    Função de votação, criada para calcular idade
+    e se pode votar ou não.
+    :ano > esta recebendo a data atual, expecifico o ano
+    :idade > está recebendo :ano - ano_nascimento do usuario
+    
+    """
+    
+    from datetime import datetime
+    ano = datetime.today().year
+    idade = ano - nasc
+    if idade <= 16:
+        return print(f'Você tem {idade}! VOTO NEGADO')
+    elif idade > 16 and idade <= 50:
+        return print(f'Você tem {idade}! VOTO OBRIGATÓRIO')
+    else:
+        return print(f'Você tem {idade}! VOTO OPCIONAL')
+    
+nasc = int(input('Qual o ano de nascimento?\n'))
+votacao(nasc)
 
-
-print(soma(10, 12))
